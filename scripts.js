52f50971d87c172097b5b8cb93d5558d14b61b5f
“Aline <1aline9mirian9silveira6@gmail.com>
2024-07-24 01:46:17 -0300

    js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() { 
-    //document = representa o documento HTML carregado no navegador
+    //document = representa o documento HTML carregado no navegado
     //addEventListener = usado para registrar uma função que será chamada sempre que o evento ('DOMContentLoaded') especificado é disparado.
     //evento 'DOMContentLoaded' é literalmente = quando a DOM (document = página html) for carregada
     const form = document.getElementById('conectar');
@@ -14,12 +14,24 @@ document.addEventListener('DOMContentLoaded', function() {
         const assunto = document.getElementById('assunto').value; //getElementById = captura no html o valor contido no input cujo Id = assunto (olha lá no html, esse é o id do input assunto)
         const mensagem = document.getElementById('mensagem').value; //getElementById = captura no html o valor contido no input cujo Id = mensagem (olha lá no html, esse é o id do text area mensagem)
 
-        const mailtoLink = mailto:1aline9mirian9silveira6@gmail.com?subject=${encodeURIComponent(assunto)}&body=${encodeURIComponent('Nome: ' + nome + '\n\n' + mensagem)};
+        const mailtoLink = `mailto:1aline9mirian9silveira6@gmail.com?subject=${encodeURIComponent(assunto)}&body=${encodeURIComponent('Nome: ' + nome + '\n\n' + mensagem)}`
 
          // console.log('oi' + nome + assunto + mensagem) //esse console vc descomnta se quiser ver o que é capturado em cada campo ao clicar no botão enviar
 
         console.log('mailtoLink:', mailtoLink); //console pra vc ver como está montando a mensagem
 
         window.location.href = mailtoLink; // window.location.href vai te direcionar para alguma janela, nesse caso, o endereço contido em mailtoLink
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const mailtoLink = `mailto:1aline9mirian9silveira6@gmail.com?subject=${encodeURIComponent(
+               subject.value,
+            )}&body=${encodeURIComponent(
+               'Nome: ' + recipientName.value + '\n\n' + menssagem.value,
+            )}`;
+         
+            send.href = mailtoLink;
+            document.location.href = mailtoLink;
+         });
     });
 });
